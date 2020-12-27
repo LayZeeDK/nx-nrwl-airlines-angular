1b3cac592c0481562774c007b4f91c5312b3f848
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-28 00:29:24 +0100

    feat: generate shared root state

--- libs/shared/root/data-access/src/lib/shared-root-data-access.module.ts
+++ libs/shared/root/data-access/src/lib/shared-root-data-access.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
 })
 export class SharedRootDataAccessModule {}
