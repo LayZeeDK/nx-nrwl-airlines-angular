efb618ff5b7f2d3fdf3bb2d6cc108a13065fcc94
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-28 00:22:03 +0100

    feat: generate shared-root-data-access

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/apps/booking/desktop-app',
     '<rootDir>/libs/booking/feature-shell',
     '<rootDir>/libs/booking/data-access',
+    '<rootDir>/libs/shared/root/data-access',
   ],
 };
---
+++ libs/shared/root/data-access/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/shared-root-data-access.module';
