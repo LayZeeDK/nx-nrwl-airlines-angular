41f9b6840cea94f53173c22fc0f33d87ca6638fd
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-27 20:03:44 +0100

    initial commit

---
+++ apps/booking-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-airlines-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'booking-app';
+}
