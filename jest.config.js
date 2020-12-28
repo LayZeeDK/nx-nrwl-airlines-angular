f5a0b550383a8989336f3ca41cdb1bea1bc2e6c3
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-29 00:05:40 +0100

    refactor: extract shared-root-assets

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   projects: [
     '<rootDir>/apps/booking-app',
     '<rootDir>/apps/booking/desktop-app',
-    '<rootDir>/libs/booking/feature-shell',
+    '<rootDir>/libs/booking/root/feature-shell',
     '<rootDir>/libs/booking/data-access',
     '<rootDir>/libs/shared/root/data-access',
     '<rootDir>/libs/shared/root/environments',
@@ -10,7 +10,7 @@ module.exports = {
     '<rootDir>/libs/booking/feature-flight-search',
     '<rootDir>/apps/booking/mobile-app',
     '<rootDir>/libs/check-in/data-access',
-    '<rootDir>/libs/check-in/feature-shell',
+    '<rootDir>/libs/check-in/root/feature-shell',
     '<rootDir>/apps/check-in/desktop-app',
     '<rootDir>/apps/check-in/mobile-app',
     '<rootDir>/libs/shared/seatmap/data-access',
---
+++ libs/shared/root/assets/README.md
@@ -0,0 +1,3 @@
+# shared-root-assets
+
+This library was generated with [Nx](https://nx.dev).
