264e0b473c9d56ee5ddf6a385e18b367df1e424b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-28 23:22:12 +0100

    feat: display current date in seat listing

--- libs/shared/seatmap/feature-seat-listing/src/lib/seat-listing/seat-listing.component.ts
+++ libs/shared/seatmap/feature-seat-listing/src/lib/seat-listing/seat-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@nrwl-airlines/shared/util-formatting';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'seatmap-seat-listing',
@@ -6,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './seat-listing.component.html',
 })
 export class SeatListingComponent {
+  get today(): string {
+    const now = DateTime.local();
+
+    return formatDate(now);
+  }
+
   onSeatConfirmed(isConfirmed: boolean): void {
     console.log('Is seat confirmed?', isConfirmed);
   }
